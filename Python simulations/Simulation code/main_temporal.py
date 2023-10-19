773d00d4bcb1244263ed2d3d756e6a8d4051444f
eliot-des <72158380+eliot-des@users.noreply.github.com>
2023-10-19 22:20:07 +0200

    Update main_temporal.py

--- Python simulations/Simulation code/main_temporal.py
+++ Python simulations/Simulation code/main_temporal.py
@@ -6,54 +6,59 @@ Created on Wed May 17 16:14:50 2023
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.io.wavfile import write
 from forward_methods import outputFE
 from backward_trapezoidal_methods import outputBE, outputTR 
 from rk4_methods import outputRK4
 from whaveshaping_methods import output_static
 
 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io.wavfile import write
+from forward_methods import outputFE
+from rk4_methods import outputRK4
+from backward_trapezoidal_methods import outputBE, outputTR 
+from whaveshaping_methods import output_static
+
+
 #Boss DS1 with 1N4148 diodes
 #circuit_param = np.array([2000, 0.01*10**-6, 2.52*10**-9, 25.852e-3, 1.752]) #[R, C, Is, Vt, n]
-
 
 #MXR Disto+ with 1N4148 diodes
 circuit_param = np.array([10000, 1*10**-9, 2.52*10**-9, 25.852e-3, 1.752])#[R, C, Is, Vt, n]
 
 
-A = 1 #gain applied to the signal at the input of the clipping stage
-Freq = 4000 #frequency of the input signal
+A = 2 #amplification factor of the input signal
+Freq = 500 #frequency of the sinuisoidal input signal
+tmax = 2/Freq #simulation time 
 
-
-overSamplingFactor = 32
+overSamplingFactor = 16
 Fs = 44100
 Fs*=overSamplingFactor
 
-
 Ts = 1/Fs
-tmax = 2/Freq #simulation time in second
+
 n = np.arange(round(Fs*tmax))
 
-
-
-#input amplified signal
+#input signal
 Vi = A*np.sin(2*np.pi*Freq*n*Ts)
 
-
-
+#output signal generated with different methodes
 VoTR = outputTR(Vi, Ts, circuit_param)
 VoBE = outputBE(Vi, Ts, circuit_param)
-VoFE = outputFE(Vi, Ts, circuit_param)
+VoStatic = output_static(Vi)
+
+#Not stable methods !!
+#VoFE = outputFE(Vi, Ts, circuit_param)
+#VoRK4 = outputRK4(Vi, Ts, circuit_param)
+
 
 fig, ax = plt.subplots()
 
-
 ax.plot(n*Ts, Vi,'r--',alpha=0.5, label='Input signal')
-ax.plot(n*Ts, VoTR,'k', label='Output TR')
-ax.plot(n*Ts, VoBE,'b--', label='Output BE')
-ax.plot(n*Ts, VoFE,'g', label='Output FE')
-
-
+ax.plot(n*Ts, VoTR,'k', label='Trapezoidal')
+ax.plot(n*Ts, VoBE,'b--', label='Backward Euler')
+ax.plot(n*Ts, VoStatic,'g',alpha=0.8, label='Static Approch')
 
 ax.set_title('Diode Clipping simulation with different methods')
 ax.set_xlabel('Time [s]')
